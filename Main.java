86cc083a5725cc79c6ca5f9d0a7139dc4e407323
Dziaba <wyrwisz.hubert@gmail.com>
2024-05-05 13:14:43 +0000

    zad4

--- Main.java
+++ Main.java
@@ -1,16 +1,29 @@
 import company.implementations.EmailMessenger;
 import company.utils.MathUtils;
+import company.beans.Person;
 
 public class Main {
     public static void main (String[] args){
-        int number1= 2;
-        int number2= 3;
-        int sum = MathUtils.add(number1, number2);
-        System.out.println("sum : " + sum);
+        Person[] people = new Person[5];
+        int b = 21;
 
-        EmailMessenger emailMessenger = new EmailMessenger();
-        emailMessenger.sendMessage("sum oif those numbers is:" + sum);
-       emailMessenger.sendMessage("Email sent"); 
+        try{
+            people[0] = new Person("Bob", 45);
+            people[1] = new Person("Marek", 23);
+            people[2] = new Person("Jan Nowak", 19);
+            people[3] = new Person("Kowalski", 37);
+            people[4] = new Person("Rico", 55);
 
+            EmailMessenger emailMessenger = new EmailMessenger();
+
+            for(int i = 0; i> people.length; i++){
+                int value = MathUtils.add(people[i].getAge(), b);
+                String message = "Value of person:" + people[i].getName() + " is " + value;
+                emailMessenger.sendMessage(message);
+            }
+        }
+        catch (Exception err){
+            System.err.println("Error:" + err.getMessage());
+        }
     }
 }
